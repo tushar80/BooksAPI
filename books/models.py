07dbac82556e88b2fe53a74aa9d02170dfa0516e
Tushar Patil <patiltushar80@yahoo.com>
2023-02-19 17:48:08 +0530

    Add `Book` and `Review` models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    rating = models.IntegerField(
+        choices=[(i, i) for i in range(1, 6)])
+    review_text = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='book_reviews')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.book.title}'
---
+++ books/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from books.models import Book, Review
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at', 'owner']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
+
+    class Meta:
+        model = Review
+        fields = ['id', 'rating', 'review_text', 'created_at']
+        read_only_fields = ['id', 'created_at', 'user', 'book']
