097fe1f7ac42e3250e5ab8980d5906186d4d272b
Tushar Patil <patiltushar80@yahoo.com>
2023-02-19 19:23:39 +0530

    Change timezone to IST

--- BooksAPI/settings.py
+++ BooksAPI/settings.py
@@ -133,6 +133,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+TIME_ZONE = 'Asia/Kolkata'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
