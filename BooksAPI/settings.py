ea94b0c128c8bf4b23b2a0032ff0c09f5b70446a
Tushar Patil <patiltushar80@yahoo.com>
2023-02-19 14:50:10 +0530

    Change database settings to connect to postgres

--- BooksAPI/settings.py
+++ BooksAPI/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'BooksAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': env('DB_HOST'),
+        'USER': env('DB_USERNAME'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'NAME': env('DB_NAME'),
     }
 }
 
